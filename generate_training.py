abf7d3e5a0041a97f1c3fa0a25880eb5a9d2cc35
hassan-shap <hassan.shapp@gmail.com>
2021-01-06 08:04:57 +0000

    training data N=14

--- generate_training.py
+++ generate_training.py
@@ -7,7 +7,7 @@ import time
 from numpy.linalg import matrix_power
 
 N = 14
-bond_dims  = [2,4,8,16,32,64,999]
+bond_dims  = [8,64,999] #[2,4,8,16,32,64,999]
 runs = 100
 
 with open('training_data_N_%d_eff.csv' % (N), mode='a') as training_data:
@@ -16,7 +16,7 @@ with open('training_data_N_%d_eff.csv' % (N), mode='a') as training_data:
         t = time.time()
         dims = [2] * N
         D = prod(dims)
-        NAs = np.arange(2, N+1)
+        NAs = np.arange(2, int(3*N/4),2)# np.arange(2, N+1)
         for bond_dim in bond_dims:
             if bond_dim == 999:
                 
@@ -29,7 +29,7 @@ with open('training_data_N_%d_eff.csv' % (N), mode='a') as training_data:
                 NB = int(N - NA)
                 randstate = ptr(randket, dims, np.arange(NA))
                 if NA<N:
-                    NA1s = np.arange(1, NA)
+                    NA1s = np.arange(1, NA, 2) #np.arange(1, NA)
                     for NA1 in NA1s:
                         NA2 = int(NA - NA1)
 
@@ -84,7 +84,7 @@ with open('training_data_N_%d_eff.csv' % (N), mode='a') as training_data:
                         training_writer.writerow([NA1,NA2,NB,p2,p3,p4,p5,p6,p7,p8,LN])
 
         print('Round:',i+1, round(time.time()-t,3), 'seconds')
-    
-    
+
+
 print("Done!")
 
